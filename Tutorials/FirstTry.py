1a2fcf1f19772027145b6d9c0aea52ff38acf872
Guilherme Theis <guilhermetheis@localhost.localdomain>
2019-05-23 23:07:28 +0200

    crazy joe

--- Tutorials/FirstTry.py
+++ Tutorials/FirstTry.py
@@ -1,32 +1,44 @@
 import turtle #native already
+import random
 
-def RacamanSequence(current, joe): #creating a function
+def RacamanSequence(current, joe, size, maxScale): #creating a function
     seen = set() #starting!
-    for step_size in range(1,100):
+    
+    for step_size in range(1,size):
 
+        scale = random.randrange(0, maxScale+1) #creating random scale
         backwards = current - step_size #renewing backwards with the actual step and old value
         
         if backwards > 0 and backwards not in seen:
-            joe.backward(step_size)
+            joe.setheading(90) #head point up
+            joe.circle(scale*step_size/2, 180) #drawing semicirles this why 180
             current = backwards
             seen.add(current) #adding value to a set so we can see if we saw already
         
     # Otherwise, go forwards
         else:
-            joe.forward(step_size)
+            joe.setheading(270) #head point down
+            joe.circle(scale*step_size/2, 180) #semicircle again
             current += step_size
             seen.add(current) #adding value to a set so we can see if we saw already
 
 
 window = turtle.Screen() #creates the Screen
+window.setup(width=1280, height=720, startx=10, starty=0.5) #set screen size
 
 joe = turtle.Turtle() #creates joe
-joe.shape('turtle')#cute trick
+joe.shape('circle')#cute trick
+joe.penup()
+joe.setpos(-390, 0)
+joe.pendown()
+
+stepSize = 100 #maxStepSize
+maxScale = 10 #max Multiply Scale
 
 #Creating the Racáman sequence
 
 currentNumber = 0 #start from zero (less than zero is not possible)
 
-RacamanSequence(currentNumber,joe)
+RacamanSequence(currentNumber,joe, stepSize, maxScale)
 
 turtle.done() #keeps the window from closing
