96baa78f396f35bc967fbe7b30cb5bcbc8c32cc7
Guilherme Theis <guilhermetheis@localhost.localdomain>
2019-05-23 22:11:51 +0200

    first version of Joe

---
+++ Tutorials/FirstTry.py
@@ -0,0 +1,12 @@
+import turtle #native already
+
+window = turtle.Screen() #creates the Screen
+
+joe = turtle.Turtle() #creates joe
+joe.shape('turtle')#cute trick
+
+joe.forward(500) #makes the dot go 50 pixels forward
+joe.left(250) #dot 90 pixels left
+joe.forward(300)
+
+turtle.done() #keeps the window from closing
